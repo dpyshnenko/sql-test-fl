99293c139be55a0ed7ecab4f8bb70370ab4ac242
Dmitrii Pyshnenko <dvpyshnenko@gmail.com>
2024-09-16 13:19:11 +0200

    update config

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name='sql_test',
+    name='sql_course',
     version='1.0.0',
-    url='https://github.com/uplimit/course-intermediate-sql',
+    url='https://github.com/dpyshnenko/sql-test-fl',
     license='GPL3',
-    packages=['sql_test'],
-    package_dir={'sql_test': 'sql_test'},
+    packages=['sql_course'],
+    package_dir={'sql_course': 'sql_course'},
     include_package_data=True,
     package_data={'': ['data/*.csv', "data/*.sql"]},
     install_requires=['pandas'],
